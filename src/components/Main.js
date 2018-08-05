a23bf1d19cfca833229439d60c18df605551494e
Tom Lau <tom@tomlau.local>
2018-08-05 16:38:11 +1000

    Add images & construct base div

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,25 @@ require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+/**
+ * Get Images metadata
+ */
+let imageData = require('../data/imageData.json');
+
+// add url field to each image
+for (let singleImage of imageData) {
+  singleImage.imageUrl = require('../images/' + singleImage.fileName);
+}
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec">
+        </section>
+        <nav className="controller-nav">
+        </nav>
+      </section>
     );
   }
 }
